9c043e14c7a40730653e24f993c9c75200b1881b
Krisztián  Gödrei <godreikrisztian@gmail.com>
2018-04-14 22:55:19 +0200

    step interface

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 
+	"github.com/bitrise-io/go-utils/log"
 	"github.com/godrei/go-echo/echo"
 	"github.com/spf13/cobra"
 )
@@ -11,13 +12,16 @@ var cmd = &cobra.Command{
 	Use:   "go-echo <message>",
 	Short: "Prints a given message.",
 	Run: func(cmd *cobra.Command, args []string) {
-		message := args[0]
+		message := os.Getenv("message")
+		if message == "" && len(args) > 0 {
+			message = args[0]
+		}
+		if message == "" {
+			log.Errorf("message to print not specified")
+			os.Exit(1)
+		}
 		echo.Log(message)
 	},
-}
-
-func init() {
-	cmd.Args = cobra.MinimumNArgs(1)
 }
 
 func main() {
